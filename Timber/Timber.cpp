fac42078ccd862d446c68e341eef853773f1241f
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-10-20 13:43:59 -0300

    Agrego nubes y abejas y le doy el movimiento a la abeja.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -3,6 +3,10 @@
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
+/// <summary>
+/// Mains this instance.
+/// </summary>
+/// <returns></returns>
 int main() {
 	//Crea el objeto videomode.
 	VideoMode vm(1920, 1080);
@@ -25,10 +29,60 @@ int main() {
 	//Le pide al background que ocupe toda la pantalla.
 	spriteBackground.setPosition(0, 0);
 
-	//Input del jugador.
-	while (window.isOpen()) {
-		
+	//--------Hace un sprite de un arbol--------
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
 
+	//--------Hace el sprite de la abeja--------
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 400);
+
+	//Movimiento de la abeja
+	bool beeActive = false;
+
+	//Velocidad de la abeja
+	float beeSpeed = 0.0f;
+
+	//--------Hace el sprite de las nubes--------
+	Texture textureCloud;
+
+	//Cargo la textura
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	//Las posiciono en diferentes alturas
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 100);
+	spriteCloud3.setPosition(0, 200);
+
+	//Se fija si están en pantalla
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	//Movimiento de las nubes
+
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	//Inicializa una variable reloj (para controlar las velocidades).
+	Clock clock;
+
+	while (window.isOpen()) {
+		//Input del jugador.
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 			window.close();
 
@@ -38,23 +92,61 @@ int main() {
 		****************************************
 		*/
 
-	
+		//Diferencia de tiempo entre dos actualizaciones.
+		Time deltaTime = clock.restart();
+
+		//Configurar la abeja
+		if (!beeActive) {
+			//Velocidad de la abeja.
+			srand((int)time(0));
+			beeSpeed = (rand() % 200) + 200;
+
+			//Altura de la abeja
+			srand((int)time(0) * 10);
+			float height = (rand() % 500) + 500;
+			spriteBee.setPosition(2000, height);
+
+			beeActive = true;
+		}
+		else
+			//Mover la abeja
+		{
+			spriteBee.setPosition(
+				spriteBee.getPosition().x - (beeSpeed * deltaTime.asSeconds()),
+				spriteBee.getPosition().y);
+
+			//Verificar si llego al borde de la pantalla.
+			if (spriteBee.getPosition().x < -100)
+			{
+				//La desactivo para que dibuje un nuevo frame
+				beeActive = false;
+			}
+		}
 
 		/*
 		****************************************
 		Dibujar la escena de juego
 		****************************************
 		*/
-		
-		// Limpia todo desde el ultimo frame
+
+		// Limpia todo desde el ultimo frame.
 		window.clear();
 		// Dibuja la escena
 		window.draw(spriteBackground);
+
+		//Dibuja las nubes
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
+
+		//Dibuja el arbol
+		window.draw(spriteTree);
+
+		//Dibuja las abejas
+		window.draw(spriteBee);
+
 		// Muestra lo que dibujamos
 		window.display();
-		
-
-		
 	}
 
 	return 0;
