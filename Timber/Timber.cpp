843799f849ae14754d68f67f365c9520f2fde7a0
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-10-20 23:41:20 -0300

    Se agrega movimiento a las nubes

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,4 +1,3 @@
-// This is where our game starts from
 
 #include <SFML/Graphics.hpp>
 
@@ -9,7 +8,7 @@ using namespace sf;
 /// <returns></returns>
 int main() {
 	//Crea el objeto videomode.
-	VideoMode vm(1920, 1080);
+	VideoMode vm(1600, 900);
 
 	//Crea y abre una ventana para el juego.
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
@@ -34,25 +33,25 @@ int main() {
 	textureTree.loadFromFile("graphics/tree.png");
 	Sprite spriteTree;
 	spriteTree.setTexture(textureTree);
-	spriteTree.setPosition(810, 0);
+	spriteTree.setPosition(650, 0);
 
 	//--------Hace el sprite de la abeja--------
 	Texture textureBee;
 	textureBee.loadFromFile("graphics/bee.png");
 	Sprite spriteBee;
 	spriteBee.setTexture(textureBee);
-	spriteBee.setPosition(0, 400);
+	spriteBee.setPosition(0, 200);
 
-	//Movimiento de la abeja
+	//Movimiento de la abeja.
 	bool beeActive = false;
 
-	//Velocidad de la abeja
+	//Velocidad de la abeja.
 	float beeSpeed = 0.0f;
 
 	//--------Hace el sprite de las nubes--------
 	Texture textureCloud;
 
-	//Cargo la textura
+	//Cargo la textura.
 	textureCloud.loadFromFile("graphics/cloud.png");
 	Sprite spriteCloud1;
 	Sprite spriteCloud2;
@@ -62,17 +61,17 @@ int main() {
 	spriteCloud2.setTexture(textureCloud);
 	spriteCloud3.setTexture(textureCloud);
 
-	//Las posiciono en diferentes alturas
+	//Las posiciono en diferentes alturas.
 	spriteCloud1.setPosition(0, 0);
 	spriteCloud2.setPosition(0, 100);
 	spriteCloud3.setPosition(0, 200);
 
-	//Se fija si están en pantalla
+	//Se fija si están en pantalla.
 	bool cloud1Active = false;
 	bool cloud2Active = false;
 	bool cloud3Active = false;
 
-	//Movimiento de las nubes
+	//Movimiento de las nubes.
 
 	float cloud1Speed = 0.0f;
 	float cloud2Speed = 0.0f;
@@ -95,13 +94,13 @@ int main() {
 		//Diferencia de tiempo entre dos actualizaciones.
 		Time deltaTime = clock.restart();
 
-		//Configurar la abeja
+		//Configurar la abeja.
 		if (!beeActive) {
 			//Velocidad de la abeja.
 			srand((int)time(0));
 			beeSpeed = (rand() % 200) + 200;
 
-			//Altura de la abeja
+			//Altura de la abeja.
 			srand((int)time(0) * 10);
 			float height = (rand() % 500) + 500;
 			spriteBee.setPosition(2000, height);
@@ -109,7 +108,7 @@ int main() {
 			beeActive = true;
 		}
 		else
-			//Mover la abeja
+			//Mover la abeja.
 		{
 			spriteBee.setPosition(
 				spriteBee.getPosition().x - (beeSpeed * deltaTime.asSeconds()),
@@ -118,8 +117,79 @@ int main() {
 			//Verificar si llego al borde de la pantalla.
 			if (spriteBee.getPosition().x < -100)
 			{
-				//La desactivo para que dibuje un nuevo frame
+				//La desactivo para que dibuje un nuevo frame.
 				beeActive = false;
+			}
+		}
+
+		//Configuracion de las nubes.
+		//nube1
+		if (!cloud1Active)
+		{
+			//Velocidad de la nube.
+			srand((int)time(0) * 10);
+			cloud1Speed = (rand() % 200);
+
+			//Altura de la nube.
+			srand((int)time(0) * 10);
+			float height = (rand() % 150);
+			spriteCloud1.setPosition(-200, height);
+			cloud1Active = true;
+		}
+		else {
+			spriteCloud1.setPosition(
+				spriteCloud1.getPosition().x + (cloud1Speed * deltaTime.asSeconds()),
+				spriteCloud1.getPosition().y);
+			// Verifica que la nube llegue al lado derecho
+			if (spriteCloud1.getPosition().x > 1920)
+			{
+				cloud1Active = false;
+			}
+		}
+
+		if (!cloud2Active)
+		{
+			//Velocidad de la nube.
+			srand((int)time(0) * 20);
+			cloud2Speed = (rand() % 200);
+
+			//Altura de la nube.
+			srand((int)time(0) * 20);
+			float height = (rand() % 300) - 150;
+			spriteCloud2.setPosition(-200, height);
+			cloud2Active = true;
+		}
+		else {
+			spriteCloud2.setPosition(
+				spriteCloud2.getPosition().x + (cloud2Speed * deltaTime.asSeconds()),
+				spriteCloud2.getPosition().y);
+			// Verifica que la nube llegue al lado derecho
+			if (spriteCloud2.getPosition().x > 1920)
+			{
+				cloud2Active = false;
+			}
+		}
+
+		if (!cloud3Active)
+		{
+			//Velocidad de la nube.
+			srand((int)time(0) * 30);
+			cloud3Speed = (rand() % 200);
+
+			//Altura de la nube.
+			srand((int)time(0) * 30);
+			float height = (rand() % 450) - 150;
+			spriteCloud3.setPosition(-200, height);
+			cloud3Active = true;
+		}
+		else {
+			spriteCloud3.setPosition(
+				spriteCloud3.getPosition().x + (cloud3Speed * deltaTime.asSeconds()),
+				spriteCloud3.getPosition().y);
+			// Verifica que la nube llegue al lado derecho
+			if (spriteCloud3.getPosition().x > 1920)
+			{
+				cloud3Active = false;
 			}
 		}
 
