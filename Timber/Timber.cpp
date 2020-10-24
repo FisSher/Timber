610d9fc66e4eaad6871f005237205889b1345df3
Facundo Paolini <39218134+FisSher@users.noreply.github.com>
2020-10-24 01:30:55 -0300

    Se agrega la barra de tiempo y la lógica de pausar cuando el tiempo llega a 0. Próximamente se tiene que acomodar el código.

--- Timber/Timber.cpp
+++ Timber/Timber.cpp
@@ -1,6 +1,5 @@
-
 #include <SFML/Graphics.hpp>
-
+#include <sstream>
 using namespace sf;
 /// <summary>
 /// Mains this instance.
@@ -8,7 +7,7 @@ using namespace sf;
 /// <returns></returns>
 int main() {
 	//Crea el objeto videomode.
-	VideoMode vm(1600, 900);
+	VideoMode vm(1920, 1080);
 
 	//Crea y abre una ventana para el juego.
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
@@ -33,7 +32,7 @@ int main() {
 	textureTree.loadFromFile("graphics/tree.png");
 	Sprite spriteTree;
 	spriteTree.setTexture(textureTree);
-	spriteTree.setPosition(650, 0);
+	spriteTree.setPosition(820, 0);
 
 	//--------Hace el sprite de la abeja--------
 	Texture textureBee;
@@ -80,118 +79,219 @@ int main() {
 	//Inicializa una variable reloj (para controlar las velocidades).
 	Clock clock;
 
+	//Barra de tiempo
+	RectangleShape timeBar;
+	float timeBarStartWidth = 400;
+	float timeBarHeight = 80;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+	//Variable que indica si el juego está pausado.
+	bool paused = true;
+
+	//Puntaje en pantalla.
+	int score = 0;
+
+	//Campos de texto.
+	Text messageText;
+	Text scoreText;
+
+	//Fuente para el texto.
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP.ttf");
+
+	//Asignamos la fuente al mensaje.
+	messageText.setFont(font);
+	scoreText.setFont(font);
+
+	//Asigno el mensaje de la fuente.
+	messageText.setString("Presionar enter para comenzar");
+	scoreText.setString("Puntaje = 0");
+
+	//Aumentar el tamaño del texto.
+	messageText.setCharacterSize(75);
+	scoreText.setCharacterSize(100);
+
+	//Selecciona colores de texto.
+	messageText.setFillColor(Color::White);
+	messageText.setOutlineColor(Color::White);
+	scoreText.setFillColor(Color::White);
+	scoreText.setOutlineColor(Color::White);
+
+	//Posición del texto.
+	FloatRect textRect = messageText.getLocalBounds();
+
+	messageText.setOrigin(textRect.left +
+		textRect.width / 2.0f,
+		textRect.top +
+		textRect.height / 2.0f);
+
+	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+
+	scoreText.setPosition(20, 20);
+
 	while (window.isOpen()) {
 		//Input del jugador.
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 			window.close();
+
+		if (Keyboard::isKeyPressed(Keyboard::Return)) {
+			paused = false;
+
+			//Resetear tiempo y puntaje.
+			score = 0;
+			timeRemaining = 6;
+		
+		}
+			
 
 		/*
 		****************************************
 		Actualizar escena de juego
 		****************************************
 		*/
+		if (!paused) {
 
-		//Diferencia de tiempo entre dos actualizaciones.
-		Time deltaTime = clock.restart();
+		
+			//Diferencia de tiempo entre dos actualizaciones.
+			Time deltaTime = clock.restart();
+			
+			//Restar el tiempo restante.
+			timeRemaining -= deltaTime.asSeconds();
 
-		//Configurar la abeja.
-		if (!beeActive) {
-			//Velocidad de la abeja.
-			srand((int)time(0));
-			beeSpeed = (rand() % 200) + 200;
+			//Acomodar la barra de tiempo.
+			timeBar.setSize(Vector2f(timeBarWidthPerSecond * timeRemaining, timeBarHeight));
 
-			//Altura de la abeja.
-			srand((int)time(0) * 10);
-			float height = (rand() % 500) + 500;
-			spriteBee.setPosition(2000, height);
+			if (timeRemaining <= 0.0f) {
+				//pausar el juego.
+				paused = true;
 
-			beeActive = true;
-		}
-		else
-			//Mover la abeja.
-		{
-			spriteBee.setPosition(
-				spriteBee.getPosition().x - (beeSpeed * deltaTime.asSeconds()),
-				spriteBee.getPosition().y);
+				//Cambiar el texto que se muestra al jugador.
+				messageText.setString("Se acabó el tiempo!");
 
-			//Verificar si llego al borde de la pantalla.
-			if (spriteBee.getPosition().x < -100)
-			{
-				//La desactivo para que dibuje un nuevo frame.
-				beeActive = false;
+				//Reposicionar el texto con el tamaño nuevo.
+				FloatRect textRect = messageText.getLocalBounds();
+				messageText.setOrigin(textRect.left +
+					textRect.width / 2.0f,
+					textRect.top +
+					textRect.height / 2.0f);
+
+				messageText.setPosition(1090 / 2.0f, 1080 / 2.0f);
+			
 			}
-		}
 
-		//Configuracion de las nubes.
-		//nube1
-		if (!cloud1Active)
-		{
-			//Velocidad de la nube.
-			srand((int)time(0) * 10);
-			cloud1Speed = (rand() % 200);
+			//Configurar la abeja.
+			if (!beeActive) {
+				//Velocidad de la abeja.
+				srand((int)time(0));
+				beeSpeed = (rand() % 200) + 200;
 
-			//Altura de la nube.
-			srand((int)time(0) * 10);
-			float height = (rand() % 150);
-			spriteCloud1.setPosition(-200, height);
-			cloud1Active = true;
-		}
-		else {
-			spriteCloud1.setPosition(
-				spriteCloud1.getPosition().x + (cloud1Speed * deltaTime.asSeconds()),
-				spriteCloud1.getPosition().y);
-			// Verifica que la nube llegue al lado derecho
-			if (spriteCloud1.getPosition().x > 1920)
-			{
-				cloud1Active = false;
+				//Altura de la abeja.
+				srand((int)time(0) * 10);
+				float height = (rand() % 500) + 500;
+				spriteBee.setPosition(2000, height);
+
+				beeActive = true;
 			}
-		}
-
-		if (!cloud2Active)
-		{
-			//Velocidad de la nube.
-			srand((int)time(0) * 20);
-			cloud2Speed = (rand() % 200);
-
-			//Altura de la nube.
-			srand((int)time(0) * 20);
-			float height = (rand() % 300) - 150;
-			spriteCloud2.setPosition(-200, height);
-			cloud2Active = true;
-		}
-		else {
-			spriteCloud2.setPosition(
-				spriteCloud2.getPosition().x + (cloud2Speed * deltaTime.asSeconds()),
-				spriteCloud2.getPosition().y);
-			// Verifica que la nube llegue al lado derecho
-			if (spriteCloud2.getPosition().x > 1920)
+			else
+				//Mover la abeja.
 			{
-				cloud2Active = false;
+				spriteBee.setPosition(
+					spriteBee.getPosition().x - (beeSpeed * deltaTime.asSeconds()),
+					spriteBee.getPosition().y);
+
+				//Verificar si llego al borde de la pantalla.
+				if (spriteBee.getPosition().x < -100)
+				{
+					//La desactivo para que dibuje un nuevo frame.
+					beeActive = false;
+				}
 			}
-		}
 
-		if (!cloud3Active)
-		{
-			//Velocidad de la nube.
-			srand((int)time(0) * 30);
-			cloud3Speed = (rand() % 200);
-
-			//Altura de la nube.
-			srand((int)time(0) * 30);
-			float height = (rand() % 450) - 150;
-			spriteCloud3.setPosition(-200, height);
-			cloud3Active = true;
-		}
-		else {
-			spriteCloud3.setPosition(
-				spriteCloud3.getPosition().x + (cloud3Speed * deltaTime.asSeconds()),
-				spriteCloud3.getPosition().y);
-			// Verifica que la nube llegue al lado derecho
-			if (spriteCloud3.getPosition().x > 1920)
+			//Configuracion de las nubes.
+			//nube1
+			if (!cloud1Active)
 			{
-				cloud3Active = false;
+				//Velocidad de la nube.
+				srand((int)time(0) * 10);
+				cloud1Speed = (rand() % 200);
+
+				//Altura de la nube.
+				srand((int)time(0) * 10);
+				float height = (rand() % 150);
+				spriteCloud1.setPosition(-200, height);
+				cloud1Active = true;
 			}
-		}
+			else {
+				spriteCloud1.setPosition(
+					spriteCloud1.getPosition().x + (cloud1Speed * deltaTime.asSeconds()),
+					spriteCloud1.getPosition().y);
+				// Verifica que la nube llegue al lado derecho
+				if (spriteCloud1.getPosition().x > 1920)
+				{
+					cloud1Active = false;
+				}
+			}
+
+			if (!cloud2Active)
+			{
+				//Velocidad de la nube.
+				srand((int)time(0) * 20);
+				cloud2Speed = (rand() % 200);
+
+				//Altura de la nube.
+				srand((int)time(0) * 20);
+				float height = (rand() % 300) - 150;
+				spriteCloud2.setPosition(-200, height);
+				cloud2Active = true;
+			}
+			else {
+				spriteCloud2.setPosition(
+					spriteCloud2.getPosition().x + (cloud2Speed * deltaTime.asSeconds()),
+					spriteCloud2.getPosition().y);
+				// Verifica que la nube llegue al lado derecho
+				if (spriteCloud2.getPosition().x > 1920)
+				{
+					cloud2Active = false;
+				}
+			}
+
+			if (!cloud3Active)
+			{
+				//Velocidad de la nube.
+				srand((int)time(0) * 30);
+				cloud3Speed = (rand() % 200);
+
+				//Altura de la nube.
+				srand((int)time(0) * 30);
+				float height = (rand() % 450) - 150;
+				spriteCloud3.setPosition(-200, height);
+				cloud3Active = true;
+			}
+			else {
+				spriteCloud3.setPosition(
+					spriteCloud3.getPosition().x + (cloud3Speed * deltaTime.asSeconds()),
+					spriteCloud3.getPosition().y);
+				// Verifica que la nube llegue al lado derecho
+				if (spriteCloud3.getPosition().x > 1920)
+				{
+					cloud3Active = false;
+				}
+			}
+
+			//Actualiza el puntaje mientras el juego no esté en pausa.
+			std::stringstream ss;
+			ss << "Puntaje = " << score;
+			scoreText.setString(ss.str());
+
+
+		} //end if (!paused)
+
 
 		/*
 		****************************************
@@ -215,7 +315,17 @@ int main() {
 		//Dibuja las abejas
 		window.draw(spriteBee);
 
-		// Muestra lo que dibujamos
+		//Dibuja el puntaje
+		window.draw(scoreText);
+
+		//Dibujar la barra de tiempo.
+		window.draw(timeBar);
+
+		//Dibuja el mensaje para comenzar.
+		if (paused)
+			window.draw(messageText);
+
+		// Muestra lo que dibujamos.
 		window.display();
 	}
 
